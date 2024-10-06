292b463399314f3ae5a6b4629f4967fb42107e0a
deep123 <162825065+Deepak6523@users.noreply.github.com>
2024-10-06 14:59:01 +0530

    Update vader.py

--- vader.py
+++ vader.py
@@ -1,27 +1,46 @@
 from vader_sentiment.vader_sentiment import SentimentIntensityAnalyzer
+import matplotlib.pyplot as plt
 
 analyzer = SentimentIntensityAnalyzer()
-while(True):
+
+while True:
     print("press 1 for review : ")
     print("press any key to exit : ")
-    x=input("enter your choice == ")
+    x = input("Enter your choice: ")
     if x == '1':
-        text = input("Please enter the text for reviwe : ")
+        text = input("Please enter the text for review: ")
         sentiment = analyzer.polarity_scores(text)
-        try: 
-            
+
+        try:
+            categories = ['Very Bad Review 😡🤬', 'Bad Review 😔😩', 'Neutral 😉🤓', 'Positive 😍😘🥰', 'Highly Positive 🤩😇🥳']
+            scores = [0, 0, 0, 0, 0]  
+
             if sentiment['compound'] >= 0.075:
-                print("highly positive 🤩\n")
+                print("Highly Positive 🤩\n")
+                scores[4] += 1
             elif sentiment['compound'] >= 0.05:
-                print("positive 😍\n")
-            elif sentiment['compound'] < 0.05:
-                print("bad reviwe 😔😩\n")
+                print("Positive 😍\n")
+                scores[3] += 1
+            elif sentiment['compound'] < 0.05 and sentiment['compound'] > -0.01:
+                print("Neutral 😉🤓\n")
+                scores[2] += 1
             elif sentiment['compound'] <= -0.01:
-                print("very bad reviwe 😡🤬\n")
+                print("Bad Review 😔😩\n")
+                scores[1] += 1
             else:
-                print("neutral😉🤓/n")
+                print("Very Bad Review 😡🤬\n")
+                scores[0] += 1
+
+            plt.figure(figsize=(10, 5))
+            plt.bar(categories, scores, color=['red', 'orange', 'blue', 'green', 'purple'])
+            plt.xlabel('Sentiment Categories')
+            plt.ylabel('Number of Reviews')
+            plt.title('Sentiment Analysis Review Graph')
+            plt.show()
+
         except Exception as e:
             print(e)
     else:
-        print("exited program")
+        print("Exited program")
         break
+
